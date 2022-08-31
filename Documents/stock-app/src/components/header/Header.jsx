d899e7be6ef8537f8bea1975ffecfbba7352272c
Emmanuel Tweneboah Koduah <emmanueltweneboah71@gmail.com>
2022-08-31 17:57:54 +0000

    stock project setup and implementation of the Header component

---
+++ Documents/stock-app/src/components/header/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import styles from "./Header.module.css"
+
+import { ReactComponent as Logo } from "../../assests/icons/chart-line.svg"
+
+export const Header = () => {
+  return (
+    
+        <header className={styles.header}>
+          <div className={styles.logoContainer}>
+          <Logo className={styles.logo}>pp</Logo>
+            <span>Stock</span>
+            </div>
+          <div className={styles.userContainer}>
+            <div className={styles.user}>E</div>
+            <span >Emmanuel</span>
+          </div>
+          </header>
+  
+  )
+}
